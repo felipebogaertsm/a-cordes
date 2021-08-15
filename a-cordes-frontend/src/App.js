8a7aa2788229b97351410c4786126b49d53f39c4
felip <felipebogaerts@gmail.com>
2021-08-15 12:35:38 -0300

    0.0.5
    - Started About Us page
    - User login and logout from the frontend concluded

--- a-cordes-frontend/src/App.js
+++ a-cordes-frontend/src/App.js
@@ -7,6 +7,8 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import AboutScreen from './screens/AboutScreen';
 
 function App() {
     return (
@@ -15,6 +17,8 @@ function App() {
             <main className="py-3">
                 <Container>
                     <Route path='/' component={HomeScreen} exact />
+                    <Route path='/about' component={AboutScreen} exact />
+                    <Route path='/login' component={LoginScreen} exact />
                     <Route path='/product/:id' component={ProductScreen} exact />
                     <Route path='/cart/:id?' component={CartScreen} exact />
                 </Container>
--- a-cordes-frontend/src/components/Header.js
+++ a-cordes-frontend/src/components/Header.js
@@ -1,29 +1,61 @@
-import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Container, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
+import { logout } from '../actions/userActions'
 import React from 'react'
 
 function Header() {
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin;
+
+    const dispatch = useDispatch();
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    };
+
     return (
         <header>
         <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
             <Container>
+
                 <LinkContainer to="/">
                     <Navbar.Brand>à cordes</Navbar.Brand>
                 </LinkContainer>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <LinkContainer to="/">
-                        <Nav.Link><i className="fas fa-home px-2"></i>Home</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/login">
-                        <Nav.Link><i className="fas fa-user px-2"></i>Log in</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/cart">
-                        <Nav.Link><i className="fas fa-shopping-cart px-2"></i>Cart</Nav.Link>
-                    </LinkContainer>
-                </Nav>
+                    <Nav className="me-auto">
+                        <LinkContainer to="/">
+                            <Nav.Link><i className="fas fa-home px-2"></i>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/cart">
+                            <Nav.Link><i className="fas fa-shopping-cart px-2"></i>Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/about">
+                            <Nav.Link><i className="fas fa-arrow-circle-right px-2"></i>About us</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+
+                    <Nav>
+                        {userInfo ? (
+                            <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to='/profile'>
+                                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                                </LinkContainer>
+
+                                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        ) : (
+                            <LinkContainer to="/login">
+                                <Nav.Link><i className="fas fa-user px-2"></i>Log in</Nav.Link>
+                            </LinkContainer>
+                        )}
+                    </Nav>
                 </Navbar.Collapse>
+
             </Container>
         </Navbar>
         </header>
