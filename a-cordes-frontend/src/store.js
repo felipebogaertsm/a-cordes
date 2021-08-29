077eff4574f8acd98ec2f21d527907d3e1f2af0e
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-08-29 19:01:44 -0300

    0.0.9
    - User profile on the frontend

--- a-cordes-frontend/src/store.js
+++ a-cordes-frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -12,6 +12,7 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userLoginReducer,
     userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
