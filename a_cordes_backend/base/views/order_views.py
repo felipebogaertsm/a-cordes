fe23fa04f705079ba981fcf7d4b6f957167a2a8f
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-02 20:40:52 -0300

    0.0.15
    - Get order by ID endpoint completed

--- a_cordes_backend/base/views/order_views.py
+++ a_cordes_backend/base/views/order_views.py
@@ -56,3 +56,25 @@ def add_order_items(request):
 
         serializer = OrderSerializer(order, many=False)
         return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_order_by_id(request, pk):
+    user = request.user
+
+    try:
+        order = Order.objects.get(_id=pk)
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:  # if user is not authenticated to see the particular order:
+            Response(
+                {'detail': 'Not authorized'},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+    except:
+        return Response(
+            {'detail': 'Order does not exist'},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
