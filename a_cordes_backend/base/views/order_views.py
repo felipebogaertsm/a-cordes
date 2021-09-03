b8a426df425b201f2a7ecb7937c1247f1663dccb
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-03 08:53:02 -0300

    0.0.18
    - Order pay functionality from the backend

--- a_cordes_backend/base/views/order_views.py
+++ a_cordes_backend/base/views/order_views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from datetime import datetime
 
 from base.models import *
 from base.serializers import *
@@ -78,3 +79,15 @@ def get_order_by_id(request, pk):
             {'detail': 'Order does not exist'},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_order_to_paid(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+
+    return Response('Order was paid')
