017ec6f0f7de3bac684772190565fb2bdc7ae5ec
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-03 08:48:58 -0300

    0.0.17
    - Order screen

--- a-cordes-frontend/src/App.js
+++ a-cordes-frontend/src/App.js
@@ -13,6 +13,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 import AboutScreen from './screens/AboutScreen';
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
                     <Route path='/shipping' component={ShippingScreen} exact />
                     <Route path='/payment' component={PaymentScreen} exact />
                     <Route path='/placeorder' component={PlaceOrderScreen} exact />
+                    <Route path='/order/:id' component={OrderScreen} exact />
                     <Route path='/product/:id' component={ProductScreen} exact />
                     <Route path='/cart/:id?' component={CartScreen} exact />
                 </Container>
