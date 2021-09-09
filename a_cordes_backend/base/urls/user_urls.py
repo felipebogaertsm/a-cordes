1fb9802c1f6ef30917d008b216d30402e63b0618
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-09 08:32:40 -0300

    0.0.25
    - Get user by id endpoint
    - Update user endpoint

--- a_cordes_backend/base/urls/user_urls.py
+++ a_cordes_backend/base/urls/user_urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('profile/', views.get_user_profile, name='users_profile'),
     path('profile/update/', views.update_user_profile, name='user_profile_update'),
 
+    path('<str:pk>/', views.get_user_by_id, name='user'),
+    path('update/<str:pk>/', views.update_user, name='user_update'),
     path('delete/<str:pk>/', views.delete_user, name='user_delete'),
 ]
