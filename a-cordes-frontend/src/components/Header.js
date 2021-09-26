0ead4c43c8c0b9c84a8b21ca06ce60b7a8d316a3
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-26 09:01:36 -0300

    0.0.40
    - Product search functionality

--- a-cordes-frontend/src/components/Header.js
+++ a-cordes-frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import { Navbar, Nav, NavDropdown, Container, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
+import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
 import React from 'react'
 
@@ -17,61 +18,63 @@ function Header() {
 
     return (
         <header>
-        <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
-            <Container>
+            <Navbar bg="dark" variant="dark" collapseOnSelect expand="lg">
+                <Container>
 
-                <LinkContainer to="/">
-                    <Navbar.Brand>à cordes</Navbar.Brand>
-                </LinkContainer>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>à cordes</Navbar.Brand>
+                    </LinkContainer>
 
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <LinkContainer to="/">
-                            <Nav.Link><i className="fas fa-home px-2"></i>Home</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/cart">
-                            <Nav.Link><i className="fas fa-shopping-cart px-2"></i>Cart</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/about">
-                            <Nav.Link><i className="fas fa-arrow-circle-right px-2"></i>About us</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <SearchBox />
 
-                    <Nav>
-                        {userInfo ? (
-                            <NavDropdown title={userInfo.name} id='username'>
-                                <LinkContainer to='/profile'>
-                                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                                </LinkContainer>
-
-                                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
-                            </NavDropdown>
-                        ) : (
-                            <LinkContainer to="/login">
-                                <Nav.Link><i className="fas fa-user px-2"></i>Log in</Nav.Link>
+                        <Nav className="me-auto">
+                            <LinkContainer to="/">
+                                <Nav.Link><i className="fas fa-home px-2"></i>Home</Nav.Link>
                             </LinkContainer>
-                        )}
+                            <LinkContainer to="/cart">
+                                <Nav.Link><i className="fas fa-shopping-cart px-2"></i>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/about">
+                                <Nav.Link><i className="fas fa-arrow-circle-right px-2"></i>About us</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
 
-                        {userInfo && userInfo.is_admin && (
-                            <NavDropdown title='Admin' id='adminmenu'>
-                                <LinkContainer to='/admin/userList'>
-                                    <NavDropdown.Item>Users</NavDropdown.Item>
-                                </LinkContainer>
-                                <LinkContainer to='/admin/productList'>
-                                    <NavDropdown.Item>Products</NavDropdown.Item>
-                                </LinkContainer>
-                                <LinkContainer to='/admin/orderList'>
-                                    <NavDropdown.Item>Orders</NavDropdown.Item>
-                                </LinkContainer>
-                            </NavDropdown>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
+                        <Nav>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
 
-            </Container>
-        </Navbar>
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to="/login">
+                                    <Nav.Link><i className="fas fa-user px-2"></i>Log in</Nav.Link>
+                                </LinkContainer>
+                            )}
+
+                            {userInfo && userInfo.is_admin && (
+                                <NavDropdown title='Admin' id='adminmenu'>
+                                    <LinkContainer to='/admin/userList'>
+                                        <NavDropdown.Item>Users</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/productList'>
+                                        <NavDropdown.Item>Products</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <LinkContainer to='/admin/orderList'>
+                                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                                    </LinkContainer>
+                                </NavDropdown>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
         </header>
     )
 }
--- a-cordes-frontend/src/screens/HomeScreen.js
+++ a-cordes-frontend/src/screens/HomeScreen.js
@@ -6,31 +6,32 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { listProducts } from '../actions/productActions';
 
-function HomeScreen() {
-    const dispatch = useDispatch();
-    const productList = useSelector(state => state.productList);
-    const { error, loading, products } = productList;
+function HomeScreen({ history }) {
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList);
+  const { error, loading, products } = productList;
 
-    useEffect(() => {
-        dispatch(listProducts());
-    }, [dispatch]);
+  let keyword = history.location.search
+  useEffect(() => {
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
-    return (
-        <div>
-          <h1>Latest products</h1>
-          {loading ? <Loader />
-            : error ? <Message variant='danger'>{error}</Message>
-                :
-                <Row>
-                  {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                      <Product product={product} />
-                    </Col>
-                  ))}
-                </Row>
-        }
-        </div>
-    );
+  return (
+    <div>
+      <h1>Latest products</h1>
+      {loading ? <Loader />
+        : error ? <Message variant='danger'>{error}</Message>
+          :
+          <Row>
+            {products.map(product => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+      }
+    </div>
+  );
 }
 
 export default HomeScreen
