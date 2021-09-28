94f38ee7fea44e8fc699fd1382a38e3640f7ea0c
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-28 13:56:26 -0300

    0.0.43
    - Top product endpoint

--- a_cordes_backend/base/urls/product_urls.py
+++ a_cordes_backend/base/urls/product_urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from base.views import product_views as views
 
 urlpatterns = [
-    path('', views.get_products, name='products'),
-    path('create/', views.create_product, name='create_product'),
-    path('upload/', views.upload_image, name='image_upload'),
-
-    path('<str:pk>/reviews/', views.create_product_review, name='create_review'),
-    path('<str:pk>/', views.get_product, name='product'),
-
-    path('update/<str:pk>/', views.update_product, name='update_product'),
-    path('delete/<str:pk>/', views.delete_product, name='delete_product'),
+    path("", views.get_products, name="products"),
+    path("create/", views.create_product, name="create_product"),
+    path("upload/", views.upload_image, name="image_upload"),
+    path(
+        "<str:pk>/reviews/", views.create_product_review, name="create_review"
+    ),
+    path("top/", views.get_top_products, name="top_product"),
+    path("<str:pk>/", views.get_product, name="product"),
+    path("update/<str:pk>/", views.update_product, name="update_product"),
+    path("delete/<str:pk>/", views.delete_product, name="delete_product"),
 ]
