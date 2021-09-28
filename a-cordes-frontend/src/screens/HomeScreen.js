db7230a5749fd4a7668bad54f93121812ed250c2
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-28 14:18:50 -0300

    0.0.44
    - Frontend carousel complete

--- a-cordes-frontend/src/screens/HomeScreen.js
+++ a-cordes-frontend/src/screens/HomeScreen.js
@@ -5,6 +5,7 @@ import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
 import { listProducts } from '../actions/productActions';
 
 function HomeScreen({ history }) {
@@ -19,6 +20,11 @@ function HomeScreen({ history }) {
 
     return (
         <div>
+
+            {!keyword && (
+                <ProductCarousel />
+            )}
+
             <h1>Latest products</h1>
             {loading ? <Loader />
                 : error ? <Message variant='danger'>{error}</Message>
