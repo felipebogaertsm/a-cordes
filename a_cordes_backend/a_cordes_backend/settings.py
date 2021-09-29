e2a981a64a4449fdcbb8f6309cf82f74ecbb8f9f
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-29 08:23:24 -0300

    0.1.0
    - Added allowed hosts

--- a_cordes_backend/a_cordes_backend/settings.py
+++ a_cordes_backend/a_cordes_backend/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "www.acordes.shop",
+    "acordes.shop",
+]
 
 
 # Application definition
