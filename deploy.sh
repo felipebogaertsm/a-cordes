811f02687c3388e0625c3dc6bc62caf59344c468
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-29 12:15:28 -0300

    0.1.2

--- deploy.sh
+++ deploy.sh
@@ -26,10 +26,6 @@ sudo mv ~/A-Cordes/a_cordes_backend/a_cordes_backend/settings_prod.py ~/A-Cordes
 
 sudo chown -R felipe:www-data ~/A-Cordes/
 
-# Collecting static files for Django:
-cd ~/A-Cordes/a_cordes_backend
-python manage.py collectstatic
-
 cd ~
 
 # Starting Nginx server
