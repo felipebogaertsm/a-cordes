5a59ce93f1539d2d176fb62475ae5b1c7bc8cce7
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-30 08:21:39 -0300

    0.1.3

--- a_cordes_backend/a_cordes_backend/settings.py
+++ a_cordes_backend/a_cordes_backend/settings.py
@@ -160,7 +160,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
 MEDIA_URL = "/images/"
 
 STATICFILES_DIRS = [
@@ -168,7 +167,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "a-cordes-frontend/build/static",
 ]
 
-MEDIA_ROOT = "static/images"
+MEDIA_ROOT = BASE_DIR / "static/images"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
