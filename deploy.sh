3e4fd52c8548a250108a15949c6091be179d60eb
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-09-30 13:37:50 -0300

    0.1.3

--- deploy.sh
+++ deploy.sh
@@ -8,29 +8,29 @@
 sudo systemctl stop nginx
 
 # Moving important files to the user folder
-cd ~/
-sudo mv ~/A-Cordes/a_cordes_backend/static ~/
-sudo mv ~/A-Cordes/a_cordes_backend/staticfiles ~/
+cd /home/felipe
+sudo mv /home/felipe/A-Cordes/a_cordes_backend/static ~/
+sudo mv /home/felipe/A-Cordes/a_cordes_backend/staticfiles ~/
 
 # Activating venv
 source /home/felipe/env/bin/activate
 
 # Deleting the project folder
-sudo rm -rf ~/A-Cordes/
+sudo rm -rf /home/felipe/A-Cordes/
 
 # Getting updated version from GitHub
 git clone https://github.com/felipebogaertsm/A-Cordes.git
 
 # Removing dev settings.py
-sudo rm -rf ~/A-Cordes/a_cordes_backend/a_cordes_backend/settings.py
+sudo rm -rf /home/felipe/A-Cordes/a_cordes_backend/a_cordes_backend/settings.py
 # Removing dev static files
-sudo rm -rf ~/A-Cordes/a_cordes_backend/static
-sudo rm -rf ~/A-Cordes/a_cordes_backend/staticfiles
+sudo rm -rf /home/felipe/A-Cordes/a_cordes_backend/static
+sudo rm -rf /home/felipe/A-Cordes/a_cordes_backend/staticfiles
 
 # Moving important files from user folder to their respective folders
-sudo mv ~/A-Cordes/a_cordes_backend/a_cordes_backend/settings_prod.py ~/A-Cordes/a_cordes_backend/a_cordes_backend/settings.py
-sudo mv ~/static ~/A-Cordes/a_cordes_backend/
-sudo mv ~/staticfiles ~/A-Cordes/a_cordes_backend/
+sudo mv /home/felipe/A-Cordes/a_cordes_backend/a_cordes_backend/settings_prod.py /home/felipe/A-Cordes/a_cordes_backend/a_cordes_backend/settings.py
+sudo mv /home/felipe/static /home/felipe/A-Cordes/a_cordes_backend/
+sudo mv /home/felipe/staticfiles /home/felipe/A-Cordes/a_cordes_backend/
 
 sudo chown -R felipe:www-data ~/A-Cordes/
 
