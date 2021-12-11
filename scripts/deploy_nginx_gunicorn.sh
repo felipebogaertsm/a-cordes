530240da036e3915178c71fa3ab2b0834a69de52
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-12-11 15:50:11 -0300

    0.3.14

--- scripts/deploy_nginx_gunicorn.sh
+++ scripts/deploy_nginx_gunicorn.sh
@@ -15,7 +15,7 @@ cd /home/admin
 source /home/admin/.venvs/a_cordes/bin/activate
 
 # Git pulling
-cd /home/admin
+cd /home/admin/projects
 sudo rm -rf /home/admin/projects/A-Cordes
 git clone https://felipebogaertsm@github.com/felipebogaertsm/A-Cordes.git
 
