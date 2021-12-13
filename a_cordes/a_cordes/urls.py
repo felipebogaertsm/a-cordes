59f1ce14d887cf1529d6b43bb78cb4ec99435d66
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-12-12 22:52:31 -0300

    0.3.16
    Fixed static for production

--- a_cordes/a_cordes/urls.py
+++ a_cordes/a_cordes/urls.py
@@ -34,6 +34,3 @@ urlpatterns = [
     path("api/products/", include("apps.products.urls")),
     path("api/orders/", include("apps.orders.urls")),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
