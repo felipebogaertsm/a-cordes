3c8a1b5a8c379c464c44205b2ad89b98fca04617
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-12-13 09:02:51 -0300

    0.3.17

--- a_cordes/a_cordes/settings.py
+++ a_cordes/a_cordes/settings.py
@@ -196,7 +196,6 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / "media"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
