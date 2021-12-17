f0573a7be078612f2abf2ef3a3cac73a2c5ecfb5
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2021-12-16 22:48:47 -0300

    0.3.34

--- a_cordes/a_cordes/settings.py
+++ a_cordes/a_cordes/settings.py
@@ -200,6 +200,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Setting up Linode's Object Storage
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 LINODE_BUCKET = os.environ.get(
     "A_CORDES_LINODE_BUCKET", "a-cordes-static-staging"
