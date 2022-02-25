ce8acaf3ed007fde78c8bdc1c36e3867f36a355f
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-02-25 16:50:55 -0300

    0.4.53
    - File for user manager

---
+++ a_cordes/apps/accounts/managers.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Licensed as the GNU General Public License as published by the Free Software
+# Foundation, version 3.
+# Author: Felipe Bogaerts de Mattos
+# Contact me at felipe.bogaerts@engenharia.ufjf.br
+
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self,
+        email,
+        password=None,
+    ):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not password:
+            raise ValueError("User must have a valid password")
+
+        user = self.model(email=self.normalize_email(email))
+
+        user.set_password(password)
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_staff = True
+        user.is_admin = True
+        user.save(using=self._db)
+
+        return user
--- a_cordes/apps/accounts/models.py
+++ a_cordes/apps/accounts/models.py
@@ -4,49 +4,10 @@
 # Author: Felipe Bogaerts de Mattos
 # Contact me at felipe.bogaerts@engenharia.ufjf.br
 
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-
-class UserManager(BaseUserManager):
-    def create_user(
-        self,
-        email,
-        password=None,
-    ):
-        if not email:
-            raise ValueError("User must have an email address")
-        if not password:
-            raise ValueError("User must have a valid password")
-
-        user = self.model(email=self.normalize_email(email))
-
-        user.set_password(password)
-
-        user.save(using=self._db)
-
-        return user
-
-    def create_staffuser(self, email, password=None):
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_staff = True
-        user.save(using=self._db)
-
-        return user
-
-    def create_superuser(self, email, password=None):
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_staff = True
-        user.is_admin = True
-        user.save(using=self._db)
-
-        return user
+from apps.accounts.managers import UserManager
 
 
 class User(AbstractBaseUser):
