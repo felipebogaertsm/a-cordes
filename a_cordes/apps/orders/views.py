fb2298e922544352ff4ff412cccb5a569db63311
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-02-24 21:20:22 -0300

    0.4.49
    Fixed bug

--- a_cordes/apps/orders/views.py
+++ a_cordes/apps/orders/views.py
@@ -11,7 +11,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.generics import APIView
+from rest_framework.views import APIView
 
 from apps.orders.models import *
 from apps.orders.serializers import *
