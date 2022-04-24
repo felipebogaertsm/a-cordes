3d3b843f2687d8afba205d3410026ea64b514ce4
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-24 17:27:21 -0300

    0.7.4
    Fixed navbar dropdown

--- frontend/components/NavbarUserDropdown/index.js
+++ frontend/components/NavbarUserDropdown/index.js
@@ -34,7 +34,7 @@ export default function NavbarUserDropdown() {
 
                 <p className="my-auto">User</p>
 
-                <div className="hidden md:group-hover:block absolute top-10 right-0 fade-in-out">
+                <div className="hidden md:group-hover:block absolute top-12 right-0 fade-in-out">
                     <div className="bg-stone-800 p-4 rounded-b-lg shadow-xl mt-2">
                         <div className="flex flex-col space-y-2 text-center">
                             <Link href={USER_SETTINGS_PAGE_ROUTE}>
