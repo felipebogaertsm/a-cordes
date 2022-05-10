a1267f5f1190c3f46a99da04476c6c03f88cc000
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-05-10 17:02:55 -0300

    0.7.91
    Changed folder structure of components

---
+++ frontend/components/modules/index.js
@@ -0,0 +1,27 @@
+import { Footer } from "./Footer"
+import { GriddedProductListing } from "./GriddedProductListing"
+import { ListItem } from "./ListItem"
+import { MakerItem } from "./MakerItem"
+import { MoreProducts } from "./MoreProducts"
+import { Navbar } from "./Navbar"
+import { NavbarItem } from "./NavbarItem"
+import { NavbarUserDropdown } from "./NavbarUserDropdown"
+import { ProductListing } from "./ProductListing"
+import { SearchInput } from "./SearchInput"
+import { ShippingAddressForm } from "./ShippingAddressForm"
+import { TotalPriceOrder } from "./TotalPriceOrder"
+
+export {
+    Footer,
+    GriddedProductListing,
+    ListItem,
+    MakerItem,
+    MoreProducts,
+    Navbar,
+    NavbarItem,
+    NavbarUserDropdown,
+    ProductListing,
+    SearchInput,
+    ShippingAddressForm,
+    TotalPriceOrder,
+}
