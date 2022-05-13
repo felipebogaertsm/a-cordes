f109d66f61261df7842104351db74e0b1e980c6c
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-05-13 13:04:24 -0300

    0.8.17
    Cart app converted tests to pytest

--- a_cordes/apps/cart/tests/test_models.py
+++ a_cordes/apps/cart/tests/test_models.py
@@ -4,6 +4,8 @@
 # Author: Felipe Bogaerts de Mattos
 # Contact me at felipe.bogaerts@engenharia.ufjf.br
 
+import pytest
+
 from django.test import TestCase
 
 from apps.accounts.models import User, SellerProfile
@@ -11,111 +13,84 @@ from apps.cart.models import CartItem
 from apps.products.models import Product
 
 
-class CartTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_superuser(
-            email="admin@email.com", password="wc4Xzsbc2.%w"
-        )
+@pytest.mark.django_db
+def test_cart_item_creation(user_1, product_1_seller_1):
+    """
+    Testing simple cart item creation.
+    """
 
-        self.seller_profile = SellerProfile.objects.create(
-            user=self.user,
-            name="Seller",
-            is_active=True,
-        )
+    cart_item_1 = CartItem.objects.create_item(
+        user=user_1, product=product_1_seller_1, quantity=1
+    )
 
-        self.product_1 = Product.objects.create(
-            seller_profile=self.seller_profile,
-            name="Product 1",
-            price="1000.00",
-            count_in_stock=10,
-        )
+    queryset = CartItem.objects.filter(user=user_1, product=product_1_seller_1)
 
-    def test_cart_item_creation(self):
-        """
-        Testing simple cart item creation.
-        """
+    assert queryset.exists()
+    assert len(queryset) == 1  # single result
+    assert cart_item_1 == queryset.first()
 
-        cart_item_1 = CartItem.objects.create_item(
-            user=self.user,
-            product=self.product_1,
-            quantity=1,
-        )
 
-        cart_item_from_query = CartItem.objects.get(
-            user=self.user, product=self.product_1
-        )
+@pytest.mark.django_db
+def test_duplicate_cart_item_creation(user_1, product_1_seller_1):
+    """
+    Testing what happens if two cart items objects get created for the
+    same user and the same product.
+    """
+    quantity_cart_item_1 = 2
+    quantity_cart_item_2 = 1
 
-        self.assertEqual(cart_item_1, cart_item_from_query)
+    CartItem.objects.create_item(
+        user=user_1,
+        product=product_1_seller_1,
+        quantity=quantity_cart_item_1,
+    )
 
-    def test_duplicate_cart_item_creation(self):
-        """
-        Testing what happens if two cart items objects get created for the
-        same user and the same product.
-        """
-        quantity_cart_item_1 = 2
-        quantity_cart_item_2 = 1
+    CartItem.objects.create_item(
+        user=user_1,
+        product=product_1_seller_1,
+        quantity=quantity_cart_item_2,
+    )
 
-        CartItem.objects.create_item(
-            user=self.user,
-            product=self.product_1,
-            quantity=1,
-        )
+    queryset = CartItem.objects.filter(user=user_1, product=product_1_seller_1)
 
-        CartItem.objects.create_item(
-            user=self.user,
-            product=self.product_1,
-            quantity=2,
-        )
+    assert len(queryset) == 1
+    assert (
+        queryset.first().quantity
+        == quantity_cart_item_1 + quantity_cart_item_2
+    )
 
-        all_cart_items = CartItem.objects.filter(user=self.user).filter(
-            product=self.product_1
-        )
 
-        self.assertEqual(len(all_cart_items), 1)
+@pytest.mark.django_db
+def test_cart_item_creation_count_in_stock(user_1, product_1_seller_1):
+    """
+    Testing how the cart item creation behaves when over or under
+    quantifying in relation to the product's count in stock.
+    """
+    cart_item = CartItem.objects.create_item(
+        user=user_1,
+        product=product_1_seller_1,
+        quantity=1,
+    )
 
-        cart_item = all_cart_items[0]
+    assert (
+        cart_item.quantity
+        == CartItem.objects.filter(user=user_1, product=product_1_seller_1)
+        .first()
+        .quantity
+    )
 
-        self.assertEqual(
-            cart_item.quantity,
-            quantity_cart_item_1 + quantity_cart_item_2,
-        )
+    cart_item = CartItem.objects.create_item(
+        user=user_1,
+        product=product_1_seller_1,
+        quantity=product_1_seller_1.count_in_stock - 1,
+    )
 
-    def test_cart_item_creation_count_in_stock(self):
-        """
-        Testing how the cart item creation behaves when over or under
-        quantifying in relation to the product's count in stock.
-        """
-        cart_item = CartItem.objects.create_item(
-            user=self.user,
-            product=self.product_1,
-            quantity=1,
-        )
+    assert cart_item.quantity == product_1_seller_1.count_in_stock
 
-        self.assertEqual(
-            cart_item.quantity,
-            CartItem.objects.filter(user=self.user)
-            .filter(product=self.product_1)[0]
-            .quantity,
-        )
+    cart_item = CartItem.objects.create_item(
+        user=user_1,
+        product=product_1_seller_1,
+        quantity=1,
+    )
 
-        cart_item = CartItem.objects.create_item(
-            user=self.user,
-            product=self.product_1,
-            quantity=self.product_1.count_in_stock - 1,
-        )
-
-        self.assertEqual(
-            cart_item.quantity,
-            self.product_1.count_in_stock,
-        )
-
-        cart_item = CartItem.objects.create_item(
-            user=self.user,
-            product=self.product_1,
-            quantity=1,
-        )
-
-        self.assertEqual(
-            cart_item.quantity,
-            self.product_1.count_in_stock,
-        )
+    assert cart_item.quantity == product_1_seller_1.count_in_stock
