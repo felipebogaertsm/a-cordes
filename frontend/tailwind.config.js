4bb280128b92546ef4f8a343551f4f32b2c19356
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-07-15 14:11:08 -0300

    New colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    mode: "jit",
     theme: {
+        colors: {
+            darkred: "#7D1F1F",
+            wood: "#3F241A",
+            primary: "#181818",
+            secondary: "#E0EDEB",
+            leaf: "#AAD8B2",
+        },
         extend: {
             fontFamily: {
                 sans: ["Raleway", "sans"],
