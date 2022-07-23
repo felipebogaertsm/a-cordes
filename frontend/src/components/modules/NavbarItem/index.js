e6313704fa72516f29703ac9eaceffe20d5cdbd1
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-07-23 00:34:31 -0300

    Maker page

--- frontend/src/components/modules/NavbarItem/index.js
+++ frontend/src/components/modules/NavbarItem/index.js
@@ -14,8 +14,8 @@ export default function NavbarItem(props) {
                     className="
                     bg-opacity-0 cursor-pointer bg-stone-200
                     hover:bg-opacity-10 uppercase tracking-wider
-                    font-light px-4 py-2 rounded-md
-                    transition-all duration-100
+                    font-light px-5 py-3 rounded-none hover:rounded-2xl
+                    transition-all duration-100 ease-out
                 "
                     onClick={props.onClick}
                 >
