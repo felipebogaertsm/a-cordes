2deabc5f46b7d5622f0e5983c4d7d930819cb7da
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-09-06 20:57:25 -0300

    Navbar design

--- frontend/src/components/modules/NavbarItem/index.js
+++ frontend/src/components/modules/NavbarItem/index.js
@@ -4,21 +4,29 @@
 // Author: Felipe Bogaerts de Mattos
 // Contact me at me@felipebm.com
 
+import { useRouter } from "next/router"
 import Link from "next/link"
 
 // Styles:
 import styles from "./NavbarItem.module.css"
 
 export default function NavbarItem(props) {
+    const router = useRouter()
+
     return (
         <div className="my-auto" {...props}>
             <Link href={props.to}>
                 <div
-                    className="
+                    className={`
                         bg-transparent cursor-pointer uppercase tracking-wider
-                        font-light rounded-none hover:rounded-2xl
+                        font-light rounded-none hover:rounded-2xl 
                         transition-all duration-100 ease-out bg-translate-y-2
-                "
+                        ${
+                            props.to === router.pathname
+                                ? "text-white font-semibold"
+                                : "text-secondary"
+                        }
+                    `}
                     onClick={props.onClick}
                 >
                     <p className={styles.content}>{props.children}</p>
