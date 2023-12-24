04b0b0630577acf015a73e9d8bf76fb78a093a36
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2023-12-24 13:49:24 -0300

    Added prisma

--- acordes/drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit/utils";
-
-export default defineConfig({
-  schema: "./schema.ts",
-  out: "./drizzle/migrations",
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
-  },
-});
