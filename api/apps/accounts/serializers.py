c44bc78a3b2196fbc03e011153ca94629ab1a594
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2024-11-28 00:11:36 -0300

    Dealer serializer

--- api/apps/accounts/serializers.py
+++ api/apps/accounts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import AccessToken
 
-from apps.accounts.models import User
+from apps.accounts.models import User, Dealer
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -22,3 +22,21 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ["id", "email", "first_name", "last_name"]
         read_only_fields = ["id"]
+
+
+class DealerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dealer
+        fields = [
+            "id",
+            "name",
+            "profile_picture",
+            "description",
+            "country_of_origin",
+            "contact_email",
+            "contact_phone",
+            "created_at",
+            "updated_at",
+            "users",
+        ]
+        read_only_fields = ["created_at", "updated_at"]
