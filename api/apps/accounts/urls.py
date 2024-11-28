2af2ff538efff8370bb4b09818a537e437198476
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2024-11-28 00:11:46 -0300

    Dealer view

--- api/apps/accounts/urls.py
+++ api/apps/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from apps.accounts.views import AuthViewSet, UserViewSet
+from apps.accounts.views import AuthViewSet, UserViewSet, DealerViewSet
 
 router = DefaultRouter()
 router.register(r"auth", AuthViewSet, basename="auth")
 router.register(r"users", UserViewSet, basename="user")
+router.register(r"dealers", DealerViewSet, basename="dealer")
 
 urlpatterns = [
     path("", include(router.urls)),
