5ca9f8aaf2384cceea757a58a759deff01d42951
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2024-11-27 23:58:13 -0300

    Created Dealer model

--- api/apps/accounts/models.py
+++ api/apps/accounts/models.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-from .managers import UserManager
+from apps.accounts.managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -20,3 +21,34 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self) -> str:
         return str(self.email)
+
+
+class Dealer(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("Dealer name"))
+    profile_picture = models.ImageField(
+        upload_to="dealer_profile_pictures/",
+        blank=True,
+        null=True,
+        verbose_name=_("Profile picture"),
+    )
+    description = models.TextField(blank=True, verbose_name=_("Description"))
+    country_of_origin = models.CharField(
+        max_length=100, blank=True, verbose_name=_("Country of origin")
+    )
+    contact_email = models.EmailField(blank=True, verbose_name=_("Contact email"))
+    contact_phone = models.CharField(
+        max_length=15, blank=True, verbose_name=_("Contact phone")
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created at"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated at"))
+    users = models.ManyToManyField(
+        "User", related_name="dealers", blank=True, verbose_name=_("Associated users")
+    )
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+    class Meta:
+        verbose_name = _("Dealer")
+        verbose_name_plural = _("Dealers")
+        ordering = ["-created_at"]
